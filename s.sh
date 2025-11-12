0b89c83719c2f76fb13f638cd901fc9c0f349549
ACH4Q <achaqmohammedamine1@gmail.com>
2025-11-12 14:58:17 +0100

    okey

--- s.sh
+++
@@ -1,7 +0,0 @@
-cd /goinfre/machaq
-wget https://www.python.org/ftp/python/3.10.12/Python-3.10.12.tgz
-tar -xzf Python-3.10.12.tgz
-cd Python-3.10.12
-./configure --prefix=$HOME/.local/python3.10 --enable-optimizations
-make -j4
-make install
